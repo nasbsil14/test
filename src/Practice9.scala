9eb53df459c1aeeff3041aeb6512c6a248bfbdf3
d-takehana <daisuke.takehana@opentone.co.jp>
2016-01-20 19:02:48 +0900

    up
    up

--- src/Practice9.scala
+++ src/Practice9.scala
@@ -3,10 +3,14 @@ object Practice9 {
     println("START")
 
     val cls = new Cls1 with T1
-    cls.print()
     cls.print2()
 
-    val impl = new Impl1
+    val factory = new Factory(ObjRepos)
+    factory.facade.print()
+    //ObjReposがimplicitを解決してくれるはず・・・
+    import ObjRepos._
+    val facade2 = new Facade2
+    facade2.print()
 
     println("END")
   }
@@ -17,15 +21,45 @@ object Practice9 {
       println(this.name)
     }
   }
-
   trait T1 {self: Cls1 =>
     //Clsにミックスイン前提でCls1のフィールド使う
     def print2() = {
-      println(self.name)
+      println(self.print())
     }
   }
 
+  //DIするもの
+  trait Repo {
+    def print() = println("骨")
+  }
+  class RepoImpl () extends Repo {
+    override def print() = println("肉")
+  }
+  //DI入れ物
+  trait Repos {
+    implicit val repo: Repo
+  }
+  //DI解決
+  object ObjRepos extends Repos {
+    lazy val repo = new RepoImpl()
+  }
+  //DIオブジェクトを使って処理するクラス
+  class Facade()(implicit repo: Repo){
+    def print() = repo.print()
+  }
+  class Facade2(implicit repo: Repo){
+    def print() = repo.print()
+  }
+
+  class Factory(repos : Repos){
+    // コンポーネントを列挙したクラスのフィールドをimportしてやる
+    import repos._
+
+    lazy val facade = new Facade() // 勝手に必要なコンポーネントが渡される
+  }
+
   class Impl1 () {
+    //コンパニオンオブジェクトからもらう
     implicit val name = Impl1.name
   }
   object Impl1 {
