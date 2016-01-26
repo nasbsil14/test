ab9227b93e2db7f25f20b9a9e4fb555d30bbf5a1
d-takehana <daisuke.takehana@opentone.co.jp>
2016-01-26 19:17:39 +0900

    up
    up

--- src/Practice10.scala
+++ src/Practice10.scala
@@ -1,10 +1,31 @@
+import java.io.{IOException, FileNotFoundException}
+
+import scala.io.Source
+
 object Practice10 {
   def main (args: Array[String]) {
-    val list = args.toList.map(s => addList(s))
-    println(list)
+    println("START")
+//    val list = args.toList.map(s => createCommandList(s))
+//    println(list)
+
+    val fileName = "C:/Users/d-takehana/Desktop/work/test/test.csv"
+//    Source.fromFile(fileName).foreach(print)
+    val csv =
+    try {
+      Source.fromFile(fileName).getLines().toList.map { line =>
+        println("debug:" + line)
+        line.split(",")(1)
+      }
+    } catch {
+      case ex: FileNotFoundException => println("ファイル無い")
+      case ex: IOException => println("読めない")
+    }
+
+    println(csv)
+    println("END")
   }
 
-  def addList(s: String): Command = {
+  def createCommandList(s: String): Command = {
     s match {
       case "A" =>  A
       case "B" =>  B
